675225143b508210fe393dd2ccfc8b87cec8860f
Nikhil Sharma <nikhilijs@gamil.com>
2024-10-11 02:53:01 +0530

    Your descriptive commit message

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const app = express();
+const cors = require('cors'); // Import CORS
 const { Server } = require('socket.io');
 const http = require('http');
+
+const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 const port = process.env.PORT || 5000; // Use the default port
+
+// Use CORS middleware
+app.use(cors({
+    origin: 'https://conshachat.vercel.app/', // Replace with your client URL
+    methods: ['GET', 'POST'],
+    credentials: true // Allow credentials if needed
+}));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -12,11 +21,11 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     socket.on('send name', (username) => {
-        io.emit('send name', (username));
+        io.emit('send name', username);
     });
 
     socket.on('send message', (chat) => {
-        io.emit('send message', (chat));
+        io.emit('send message', chat);
     });
 });
 
